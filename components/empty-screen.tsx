f834e053dddafc65a9444792531a84b1d848e2cf
Asharib Ali <asharibarain4@gmail.com>
2023-10-13 18:10:38 +0500

    update

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -24,7 +24,7 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Optimism AI Chatbot🤖
+          Welcome to Optimism AI Chatbot 🤖
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
           Unleash the real power of Web3 & AI with Optimism AI natively for
